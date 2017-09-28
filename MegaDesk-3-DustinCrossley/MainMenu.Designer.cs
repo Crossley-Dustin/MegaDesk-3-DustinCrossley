9e68276f32d6e466642b411e184b7ab43a145f54
Dustin Crossley <kuark7@hotmail.com>
2017-09-28 01:05:11 -0700

    Basic Desk Project created with all the required empty forms and classes.

---
+++ MegaDesk-3-DustinCrossley/MainMenu.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace MegaDesk_3_DustinCrossley
+{
+    partial class MainMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnNewQuote = new System.Windows.Forms.Button();
+            this.btnViewQuotes = new System.Windows.Forms.Button();
+            this.btnSearchQuotes = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnNewQuote
+            // 
+            this.btnNewQuote.Location = new System.Drawing.Point(68, 29);
+            this.btnNewQuote.Name = "btnNewQuote";
+            this.btnNewQuote.Size = new System.Drawing.Size(148, 50);
+            this.btnNewQuote.TabIndex = 0;
+            this.btnNewQuote.Text = "Add New Quote";
+            this.btnNewQuote.UseVisualStyleBackColor = true;
+            // 
+            // btnViewQuotes
+            // 
+            this.btnViewQuotes.Location = new System.Drawing.Point(68, 97);
+            this.btnViewQuotes.Name = "btnViewQuotes";
+            this.btnViewQuotes.Size = new System.Drawing.Size(148, 50);
+            this.btnViewQuotes.TabIndex = 1;
+            this.btnViewQuotes.Text = "View Quotes";
+            this.btnViewQuotes.UseVisualStyleBackColor = true;
+            // 
+            // btnSearchQuotes
+            // 
+            this.btnSearchQuotes.Location = new System.Drawing.Point(68, 166);
+            this.btnSearchQuotes.Name = "btnSearchQuotes";
+            this.btnSearchQuotes.Size = new System.Drawing.Size(148, 50);
+            this.btnSearchQuotes.TabIndex = 2;
+            this.btnSearchQuotes.Text = "Search Quotes";
+            this.btnSearchQuotes.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(68, 239);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(148, 50);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            // 
+            // MainMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(524, 328);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnSearchQuotes);
+            this.Controls.Add(this.btnViewQuotes);
+            this.Controls.Add(this.btnNewQuote);
+            this.Name = "MainMenu";
+            this.Text = "MegaDesk";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnNewQuote;
+        private System.Windows.Forms.Button btnViewQuotes;
+        private System.Windows.Forms.Button btnSearchQuotes;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
